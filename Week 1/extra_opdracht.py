e47986efc0357baa4675917f2ce8818d811928af
Jeroen <jeroenvanhattem@outlook.com>
2017-11-15 16:36:20 +0100

    Changed assignments 3 due to changed in task

---
+++ Week 1/extra_opdracht.py
@@ -0,0 +1,18 @@
+"""
+description
+The birthdayChances() function will calculate the chance of two people in a group of 23 sharing their birthday.
+
+Return
+------
+chance: int
+The chance two people share their birthday
+"""
+
+def conway(x, i):
+	numbers = {x: 1}
+
+	for n in range(i):
+		numbers.get()
+		
+
+conway(10, 10)
--- Week 1/opdracht_1.py
+++ Week 1/opdracht_1.py
@@ -13,10 +13,13 @@ The highest number from the list passed in the parameter.
 array = [0, 10, 4, 6, 7, 12, 15]
 
 def myMax(a):
-	highest = a[0]
-	for x in a:
-		if x > highest:
-			highest = x
-	return highest
+	if len(array) > 0:
+		highest = a[0]
+		for x in a:
+			if x > highest:
+				highest = x
+		return highest
+	else:
+		return "Array is empty"
 
 print(myMax(array)) 
